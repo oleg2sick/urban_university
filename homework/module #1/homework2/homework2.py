d4ea12774927bba3347213e2dafee543246c7722
oleg2sick <126007103+oleg2sick@users.noreply.github.com>
2024-07-09 21:23:21 +0300

    Update homework2.py
    оформил задание, добавил комментарий

--- homework/module #1/homework2/homework2.py
+++ homework/module #1/homework2/homework2.py
@@ -6,4 +6,4 @@ course = 'Python'
 
 time_per_task = time_spent / tasks_completed
 
-print('Курс: ' + course + ', всего задач: ' + str(tasks_completed) + ', затрачено часов: ' + str(time_spent) + ', среднее время выполнения ' + str(time_per_task) + ' часов' )
+print('Курс: ' + course + ', всего задач: ' + str(tasks_completed) + ', затрачено часов: ' + str(time_spent) + ', среднее время выполнения ' + str(time_per_task) + ' часов' )            #вывел строку согласно заданию
