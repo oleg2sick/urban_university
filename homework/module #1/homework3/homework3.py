91bfe11a2dc57c527c53903061dbc71cd179ce1c
oleg2sick <126007103+oleg2sick@users.noreply.github.com>
2024-07-09 21:32:19 +0300

    Add files via upload
    Added homework3.py file

---
+++ homework/module #1/homework3/homework3.py
@@ -0,0 +1,11 @@
+name = 'Oleg'
+print('Name: ' + name)
+
+age = 25
+print('Age: ' + str(age))
+
+age = age + 1
+print('New_age: ' + str(age))
+
+is_student = True
+print('Is Student: ' + str(is_student))
