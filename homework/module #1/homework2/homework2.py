a5806a2303f60585a96c57dedd0fa6c982c01baf
oleg2sick <126007103+oleg2sick@users.noreply.github.com>
2024-07-09 21:20:03 +0300

    Add files via upload

---
+++ homework/module #1/homework2/homework2.py
@@ -0,0 +1,9 @@
+tasks_completed = 12
+
+time_spent = 1.5
+
+course = 'Python'
+
+time_per_task = time_spent / tasks_completed
+
+print('Курс: ' + course + ', всего задач: ' + str(tasks_completed) + ', затрачено часов: ' + str(time_spent) + ', среднее время выполнения ' + str(time_per_task) + ' часов' )
