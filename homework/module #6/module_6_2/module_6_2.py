831bef910c50a0bcfb397b0a473e4f10d6a446e7
oleg2sick <126007103+oleg2sick@users.noreply.github.com>
2024-10-05 18:03:32 +0300

    Update module_6_2.py

--- homework/module #6/module_6_2/module_6_2.py
+++ homework/module #6/module_6_2/module_6_2.py
@@ -1,25 +1,25 @@
 class Vehicle:
     __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
-    def __init__(self, owner, __model, __color, __engine_power):
+    def __init__(self, owner, model, color, engine_power):
         self.owner = owner
-        self.__model = __model
-        self.__engine_power = __engine_power
-        self.__color = __color
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color
 
     def get_model(self):
-        print(f'Модель: {self.__model}')
+        return f'Модель: {self.__model}'
 
     def get_horsepower(self):
-        print(f'Мощность двигателя: {self.__engine_power}')
+        return f'Мощность двигателя: {self.__engine_power}'
 
     def get_color(self):
-        print(f'Цвет: {self.__color}')
+        return f'Цвет: {self.__color}'
 
     def print_info(self):
-        Vehicle.get_model(self)
-        Vehicle.get_horsepower(self)
-        Vehicle.get_color(self)
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
         print(f'Владелец: {self.owner}')
 
     def set_color(self, new_color):
@@ -34,6 +34,9 @@ class Vehicle:
 
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
+
+    def __init__(self, owner, model, color, engine_power):
+        super().__init__(owner, model, color, engine_power)
 
 # Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
