fb9241fc4ddec89ca608781b74c8c658cf363a4f
oleg2sick <126007103+oleg2sick@users.noreply.github.com>
2024-12-11 20:54:56 +0300

    Update module_12_2.py

--- homework/module #12/module_12_2/module_12_2.py
+++ homework/module #12/module_12_2/module_12_2.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.path.append('./HumanMoveTests')
+sys.path.append('./HumanMoveTest')
 
 import unittest
 import runner_and_tournament as rat
@@ -8,41 +8,40 @@ import runner_and_tournament as rat
 
 class TournamentTest(unittest.TestCase):
 
+
     @classmethod
     def setUpClass(cls):
-        global all_results
-        all_results = []
+        cls.all_results = []
 
     def setUp(self):
-        global first_runner, second_runner, third_runner
-        first_runner = rat.Runner('Усэйн', 10)
-        second_runner = rat.Runner('Андрей', 9)
-        third_runner = rat.Runner('Ник', 3)
+        self.first_runner = rat.Runner('Усэйн', 10)
+        self.second_runner = rat.Runner('Андрей', 9)
+        self.third_runner = rat.Runner('Ник', 3)
 
     def test_first_third(self):
-        first_tour = rat.Tournament(90, first_runner, third_runner)
+        first_tour = rat.Tournament(90, self.first_runner, self.third_runner)
         result = first_tour.start()
-        all_results.append(result)
+        TournamentTest.all_results.append(result)
         last_one = result[max(result.keys())]
         self.assertTrue(last_one, 'Ник')
 
     def test_second_third(self):
-        second_tour = rat.Tournament(90, second_runner, third_runner)
+        second_tour = rat.Tournament(90, self.second_runner, self.third_runner)
         result = second_tour.start()
-        all_results.append(result)
+        TournamentTest.all_results.append(result)
         last_one = result[max(result.keys())]
         self.assertTrue(last_one, 'Ник')
 
     def test_all(self):
-        third_tour = rat.Tournament(90, first_runner, second_runner, third_runner)
+        third_tour = rat.Tournament(90, self.first_runner, self.second_runner, self.third_runner)
         result = third_tour.start()
-        all_results.append(result)
+        TournamentTest.all_results.append(result)
         last_one = result[max(result.keys())]
         self.assertTrue(last_one, 'Ник')
 
     @classmethod
     def tearDownClass(cls):
-        for res in all_results:
+        for res in TournamentTest.all_results:
             print(res)
 
 if __name__ == '__main__':
