6a58727dab3d29392bc44c31fe2b3c8f005a6b99
oleg2sick <126007103+oleg2sick@users.noreply.github.com>
2024-12-20 13:35:38 +0300

    Update module_16_3.py

--- homework/module #16/module_16_3/module_16_3.py
+++ homework/module #16/module_16_3/module_16_3.py
@@ -12,19 +12,40 @@ async def get_all_users() -> dict:
 
 
 @app.post('/user/{username}/{age}')
-async def add_user(username: str, age: int) -> str:
+async def add_user(user_name: Annotated[str, Path(min_length=5,
+                                                  max_length=20,
+                                                  description='Enter username',
+                                                  example='UrbanUser')],
+                   age: Annotated [int, Path(ge=18,
+                                             le=120,
+                                             description='Enter age',
+                                             example=24)]) -> str:
     current_index = str(int(max(users, key=int)) + 1)
     users[current_index] = f'Имя: {username}, возраст: {age}'
     return f'User {current_index} is registered'
 
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: str, user_name: str, age: int) -> str:
+async def update_user(user_id: Annotated [int, Path(ge=1,
+                                                    le=100,
+                                                    description='Enter User ID',
+                                                    example=1)], 
+                      user_name: Annotated[str, Path(min_length=5,
+                                                     max_length=20,
+                                                     description='Enter username',
+                                                     example='UrbanUser')],
+                      age: Annotated [int, Path(ge=18,
+                                  le=120,
+                                  description='Enter age',
+                                  example=24)]) -> str:
     users[user_id] = f'Имя: {user_name}, возраст: {age}'
     return f'The user {user_id} is udpated'
 
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: str) -> str:
+async def delete_user(user_id: Annotated [int, Path( ge=1,
+                                                     le=100,
+                                                     description='Enter User ID',
+                                                     example=1)]) -> str:
     users.pop(user_id)
     return f'The user {user_id} was deleted'
