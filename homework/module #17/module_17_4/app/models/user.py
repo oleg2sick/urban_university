d0abe7c2e6385b434d8cc9cbb96e2a9fc60cf712
oleg2sick <126007103+oleg2sick@users.noreply.github.com>
2024-12-20 17:50:40 +0300

    Add files via upload

---
+++ homework/module #17/module_17_4/app/models/user.py
@@ -0,0 +1,19 @@
+
+from fastapi import APIRouter
+from app.backend.db import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import CreateTable
+
+
+class User(Base):
+    __tablename__ = 'users'
+    __table_args__ = {'keep_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+
+    tasks = relationship('Task', back_populates='user')
