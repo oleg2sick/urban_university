df032aaa37a5fa27c95ccd55066287bc1539b9ae
oleg2sick <126007103+oleg2sick@users.noreply.github.com>
2024-12-21 17:43:29 +0300

    Update module_16_4.py

--- homework/module #16/module_16_4/module_16_4.py
+++ homework/module #16/module_16_4/module_16_4.py
@@ -19,27 +19,27 @@ async def get_all_users() -> List[User]:
 
 
 @app.post('/user/{username}/{age}')
-async def add_user(username: str, age: int) -> str:
+async def add_user(username: str, age: int):
     current_id = max((user.id for user in users), default=0) + 1
     new_user = User(id=current_id, username=username, age=age)
     users.append(new_user)
-    return f'User {current_id} is registered'
+    return new_user
 
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: int, username: str, age: int) -> str:
+async def update_user(user_id: int, username: str, age: int):
     for user in users:
         if user.id == user_id:
             user.username = username
             user.age = age
-            return f'The user {user_id} is updated'
+            return user
     raise HTTPException(status_code=404, detail='User was not found')
 
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: int) -> str:
+async def delete_user(user_id: int):
     for user in users:
         if user.id == user_id:
             users.pop(user_id - 1)
-            return f'The user {user_id} was deleted'
+            return user
     raise HTTPException(status_code=404, detail='User was not found')
